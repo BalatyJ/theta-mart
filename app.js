c8198286d9f6c2c2856cc7fcf3ad81eafb2cf4df
Xizhu Wang <wangxiz@flip1.engr.oregonstate.edu>
2022-11-15 03:44:11 -0800

    updates

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 // app.js - SETUP section
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-PORT = 57209;
+PORT = 57206;
 
 // Database
 var db = require('./database/db-connector');

1ca0ede9aeba271986139f0afac3ff3398a64802
Xizhu Wang <wangxiz@flip1.engr.oregonstate.edu>
2022-11-14 16:53:46 -0800

    change port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 // app.js - SETUP section
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-PORT = 57206;
+PORT = 57209;
 
 // Database
 var db = require('./database/db-connector');

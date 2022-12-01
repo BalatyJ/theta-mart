d4bd20e8f1bfdd72cee52b39d2b0d5d04af056dd
BalatyJ <107899791+BalatyJ@users.noreply.github.com>
2022-12-01 00:00:29 -0800

    Update delete_orderProduct.js
    added comments

--- public/js/delete_orderProduct.js
+++ public/js/delete_orderProduct.js
@@ -1,4 +1,7 @@
 function deleteOrderProduct(orderProductID) {
+    // We set up our ajax request, where we include the orderProductID as data, and send a request to our app.js
+    // as a DELETE request. Once the request has successfully returned, the webpage is refreshed.
+
     let link = '/delete-orderproduct-ajax/';
     let data = {
         id: orderProductID

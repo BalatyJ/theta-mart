fbb455f0cf206b19c0a9c7621c3b66e46522c653
BalatyJ <107899791+BalatyJ@users.noreply.github.com>
2022-12-01 00:01:36 -0800

    Update filter_orderProduct.js
    added comments and refactored code

--- public/js/filter_orderProduct.js
+++ public/js/filter_orderProduct.js
@@ -6,21 +6,14 @@ function filterSearch() {
     let filter = searchFilter.value;
 
 
-
+    // We send a request to our express server asking for data from OrderProducts filtered by the value in filter.
     fetch(`/orderProducts?orderid=${filter}`, {
         method: 'GET'
     }).then(() => {
-        // Once the request is received, we should update the web browser's url to reflect the correct page.
+        // Once the request is received, we should update the web browser's url to the page that has the correctly rendered information.
         window.history.replaceState("orderProducts", "", `orderProducts?orderid=${filter}`)
 
-        for (let i, j = 0; i = searchFilter.options[j]; j++) {
-            if (i.value == filter) {
-                i.setAttribute("selected", "selected")
-                break;
-            }
-        }
-
-        // Then refresh the page.
+        // Then refresh the page to show the rendered page.
         window.location.reload()
     }
     )

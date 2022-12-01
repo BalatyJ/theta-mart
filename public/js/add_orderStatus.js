1fdeba20e5f458849354dca3e8061f1d65e07283
BalatyJ <107899791+BalatyJ@users.noreply.github.com>
2022-11-30 23:58:04 -0800

    Update add_orderStatus.js
    added comments and refactored code

--- public/js/add_orderStatus.js
+++ public/js/add_orderStatus.js
@@ -19,7 +19,7 @@ addorderStatusForm.addEventListener("submit", function (e) {
     // Put our data we want to send in a javascript object
     let data = {
         orderstatus_id: orderStatusValue,
-        description: descriptionValue,
+        description: descriptionValue
     }
 
     // Setup our AJAX request
@@ -29,63 +29,19 @@ addorderStatusForm.addEventListener("submit", function (e) {
 
     // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-            // Add the new data to the table
-            addRowToTable(xhttp.response);
-
-            // Clear the input fields for another transaction
-            inputorderStatus.value = '';
-            inputDescription.value = '';
-        }
-        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+        if (xhttp.readyState == 4 && xhttp.status != 201) {
             console.log("There was an error with the input.")
+            alert(`The OrderStatus "${data.orderstatus_id}" is already being used. Please pick a unique name for each new OrderStatus.`)
         }
     }
 
-
+    // Once we receive a response, we refresh the page.
     xhttp.onload = function () {
         location.reload();
-      };
-    
+    };
+
 
     // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
 
 })
-
-
-// Creates a single row from an Object representing a single record from 
-// bsg_people
-addRowToTable = (data) => {
-
-    // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById('orderstatus-table');
-
-    // Get the location where we should insert the new row (end of table)
-    let newRowIndex = currentTable.rows.length;
-
-    // Get a reference to the new row from the database query (last object)
-    let parsedData = JSON.parse(data);
-    let newRow = parsedData[parsedData.length - 1]
-
-    // Create a row and 9 cells
-    let row = document.createElement("TR");
-    let orderStatusCell = document.createElement("TD");
-    let descriptionCell = document.createElement("TD");
-
-
-    // Fill the cells with correct data
-    orderStatusCell.innerText = newRow.orderstatus_id;
-    descriptionCell.innerText = newRow.description;
-
-
-    // Add the cells to the row 
-    row.appendChild(idCell);
-    row.appendChild(orderStatusCell);
-    row.appendChild(descriptionCell);
-
-
-    // Add the row to the table
-    currentTable.appendChild(row);
-}

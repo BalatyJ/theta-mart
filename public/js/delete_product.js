ecd22f4b09ee56da996696a10d853c0179a98964
BalatyJ <107899791+BalatyJ@users.noreply.github.com>
2022-12-01 00:01:10 -0800

    Update delete_product.js
    added comments

--- public/js/delete_product.js
+++ public/js/delete_product.js
@@ -1,4 +1,8 @@
 function deleteProduct(productID) {
+  // We set up our ajax request, where we include the productID as data, and send a request to our app.js
+  // as a DELETE request. If the request successfully returns, the webpage is refreshed. Otherwise,
+  // an alert is posted letting the user know why the deletion failed.
+
   let link = '/delete-product-ajax/';
   let data = {
     id: productID

d9b18d7238d28dd14a56bacb86e6a74018278a45
BalatyJ <107899791+BalatyJ@users.noreply.github.com>
2022-12-01 00:05:34 -0800

    Update update_product.js
    added comments

--- public/js/update_product.js
+++ public/js/update_product.js
@@ -36,17 +36,12 @@ updateProductForm.addEventListener("submit", function (e) {
 
     // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-            // Add the new data to the table
-            updateRow(xhttp.response, parseInt(inputProductValue));
-
-        }
-        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+        if (xhttp.readyState == 4 && xhttp.status != 200) {
             console.log("There was an error with the input.")
         }
     }
 
+    // Refreshes the page to show the newly updated data.
     xhttp.onload = function () {
         location.reload();
     };
@@ -57,36 +52,15 @@ updateProductForm.addEventListener("submit", function (e) {
 })
 
 
-function updateRow(data, productID) {
-    let parsedData = JSON.parse(data);
-
-    let table = document.getElementById("product-table");
-
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        //iterate through rows
-        //rows would be accessed using the "row" variable assigned in the for loop
-        if (table.rows[i].getAttribute("data-value") === productID) {
-
-            let updateRowIndex = table.getElementsByTagName("tr")[i];
-
-
-            let descriptionTD = updateRowIndex.getElementsByTagName("td")[2];
-            let priceTD = updateRowIndex.getElementsByTagName("td")[3];
-            let stockTD = updateRowIndex.getElementsByTagName("td")[4];
-
-            descriptionTD.innerHTML = parsedData[0].description;
-            stockTD.innerHTML = parsedData[0].stock;
-            priceTD.innerHTML = parsedData[0].price;
-        }
-    }
-}
-
-
+// When the PK is selected for the Update form, the fields in the 
+// form get automatically selected or filled out based on the PK selected.
 function autofill() {
+    // Get the ID of our update form and the value of the PK dropdown selected.
     let selectElement = document.getElementById('input-updateproduct-p');
     let selectElement_id = selectElement.value;
 
     if (selectElement_id === '') {
+        // If the default option is selected, all the values should be set to the default value.
         document.getElementById('input-updatedescription-p').value = ''
         document.getElementById('input-updateprice-p').value = ''
         document.getElementById('input-updatestock-p').value = ''
@@ -94,10 +68,12 @@ function autofill() {
 
         let table = document.getElementById('product-table');
 
+        // Otherwise, we loop through the table, find the row corresponding to the selected PK.
         for (let i = 0, row; row = table.rows[i]; i++) {
             console.log(table.rows[i].getAttribute('data-value'));
             if (table.rows[i].getAttribute('data-value') == selectElement_id) {
 
+                // Then we update the update form's fields with the corresponding values from the table.
                 let updateRowIndex = table.getElementsByTagName("tr")[i];
 
                 let td1 = updateRowIndex.getElementsByTagName("td")[2];

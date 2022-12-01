0726cef0ce3886e17b8864340abe2325f9f641bb
BalatyJ <107899791+BalatyJ@users.noreply.github.com>
2022-11-30 23:56:24 -0800

    Update add_driver.js
    removed superfluous code and added comments.

--- public/js/add_driver.js
+++ public/js/add_driver.js
@@ -34,88 +34,18 @@ addDriverForm.addEventListener("submit", function (e) {
 
     // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-            // Add the new data to the table
-            addRowToTable(xhttp.response);
-
-            // Clear the input fields for another transaction
-            inputFirstName.value = '';
-            inputLastName.value = '';
-            inputPhone.value = '';
-            inputAvailable.value = '';
-        }
-        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+        if (xhttp.readyState == 4 && xhttp.status != 201) {
             console.log("There was an error with the input.")
         }
     }
 
-        
+    // Once we receive a response, we refresh the page.
     xhttp.onload = function () {
         location.reload();
-      };
-    
+    };
+
 
     // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
 
 })
-
-
-// Creates a single row from an Object representing a single record from 
-// bsg_people
-addRowToTable = (data) => {
-
-    // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById("person-table");
-
-    // Get the location where we should insert the new row (end of table)
-    let newRowIndex = currentTable.rows.length;
-
-    // Get a reference to the new row from the database query (last object)
-    let parsedData = JSON.parse(data);
-    let newRow = parsedData[parsedData.length - 1]
-
-    // Create a row and 9 cells
-    let row = document.createElement("TR");
-    let idCell = document.createElement("TD");
-    let firstNameCell = document.createElement("TD");
-    let lastNameCell = document.createElement("TD");
-    let phoneCell = document.createElement("TD");
-    let availableCell = document.createElement("TD");
-
-    let deleteCell = document.createElement("TD");
-
-    // Fill the cells with correct data
-    idCell.innerText = newRow.id;
-    firstNameCell.innerText = newRow.fname;
-    lastNameCell.innerText = newRow.lname;
-    phoneCell.innerText = newRow.phone;
-    availableCell.innerText = newRow.available;
-
-    deleteCell = document.createElement("button");
-    deleteCell.innerHTML = "Delete";
-    deleteCell.onclick = function () {
-        deleteDriver(newRow.id);
-    };
-
-    // Add the cells to the row 
-    row.appendChild(idCell);
-    row.appendChild(firstNameCell);
-    row.appendChild(lastNameCell);
-    row.appendChild(phoneCell);
-    row.appendChild(availableCell);
-    row.appendChild(deleteCell);
-
-    // Add a row attribute so the deleteRow function can find a newly added row
-    row.setAttribute('data-value', newRow.id);
-
-    // Add the row to the table
-    currentTable.appendChild(row);
-
-    let selectMenu = document.getElementById("update-fullname-driver");
-    let option = document.createElement("option");
-    option.text = newRow.fname + ' ' +  newRow.lname;
-    option.value = newRow.id;
-    selectMenu.add(option);
-}

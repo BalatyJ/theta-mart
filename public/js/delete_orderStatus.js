87fcd650e2419a00135f0e8a2bd484d29cad62f2
BalatyJ <107899791+BalatyJ@users.noreply.github.com>
2022-12-01 00:00:51 -0800

    Update delete_orderStatus.js
    added comments

--- public/js/delete_orderStatus.js
+++ public/js/delete_orderStatus.js
@@ -1,4 +1,8 @@
 function deleteOrderStatus(statusID) {
+  // We set up our ajax request, where we include the statusID as data, and send a request to our app.js
+  // as a DELETE request. If the request successfully returns, the webpage is refreshed. Otherwise,
+  // an alert is posted letting the user know why the deletion failed.
+
   let link = '/delete-orderStatus-ajax/';
   let data = {
     id: statusID
